4b6283b6411eb339b0e418e6c7220055afb73aed
Drew White <32739358+Drucial@users.noreply.github.com>
2024-04-26 20:03:11 -0400

    updated alpha nd colorscheme for new splash screen

--- lua/drucial/plugins/alpha.lua
+++ lua/drucial/plugins/alpha.lua
@@ -1,36 +1,81 @@
+local function configure()
+	local dashboard = require("alpha.themes.dashboard")
+	local colorscheme = require("drucial/plugins/colorscheme")
+
+	-- Now you can access the colors table
+	local colors = colorscheme.colors
+
+	-- Define custom highlight groups with colors from your theme
+	vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = colors.blue })
+	vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = colors.green })
+	vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = colors.cyan })
+	vim.api.nvim_set_hl(0, "NeovimDashboardLogo4", { fg = colors.green })
+	vim.api.nvim_set_hl(0, "NeovimDashboardLogo5", { fg = colors.blue })
+
+	dashboard.section.header.type = "group"
+	dashboard.section.header.val = {
+		{
+			type = "text",
+			val = [[  Y88b Y88  888'Y88   e88 88e   Y88b Y88  ]],
+			opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" },
+		},
+		{
+			type = "text",
+			val = [[   Y88b Y8  888 ,'Y  d888 888b   Y88b Y8  ]],
+			opts = { hl = "NeovimDashboardLogo2", shrink_margin = false, position = "center" },
+		},
+		{
+			type = "text",
+			val = [[  b Y88b Y  888C8   C8888 8888D b Y88b Y  ]],
+			opts = { hl = "NeovimDashboardLogo3", shrink_margin = false, position = "center" },
+		},
+		{
+			type = "text",
+			val = [[  8b Y88b   888 ",d  Y888 888P  8b Y88b   ]],
+			opts = { hl = "NeovimDashboardLogo4", shrink_margin = false, position = "center" },
+		},
+		{
+			type = "text",
+			val = [[  88b Y88b  888,d88   "88 88"   88b Y88b  ]],
+			opts = { hl = "NeovimDashboardLogo5", shrink_margin = false, position = "center" },
+		},
+		-- [[-------------  *  --------------]],
+		-- [[ Y8b Y88888P  888      e   e     ]],
+		-- [[  Y8b Y888P   888     d8b d8b    ]],
+		-- [[   Y8b Y8P    888    e Y8b Y8b   ]],
+		-- [[    Y8b Y     888   d8b Y8b Y8b  ]],
+		-- [[     Y8P      888  d888b Y8b Y8b ]],
+	}
+
+	dashboard.section.buttons.val = {
+		dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+		dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+		dashboard.button("g", "󰷾 " .. " Find text", ":Telescope live_grep <CR>"),
+		dashboard.button("b", " " .. " File browser", ":NvimTreeFocus <CR>"),
+		dashboard.button("r", "󰄉 " .. " Recent files", ":Telescope oldfiles <CR>"),
+		dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+		dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+		dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+	}
+
+	return dashboard.config
+end
+
 return {
-  "goolord/alpha-nvim",
-  event = "VimEnter",
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
+	"goolord/alpha-nvim",
+	lazy = true,
+	event = "VimEnter",
+	config = function(_, dashboard)
+		if vim.o.filetype == "lazy" then
+			vim.cmd.close()
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "AlphaReady",
+				callback = function()
+					require("lazy").show()
+				end,
+			})
+		end
 
-    -- Set header
-    dashboard.section.header.val = {
-      "                                                     ",
-      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-      "                                                     ",
-    }
-
-    -- Set menu
-    dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-      dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-      dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
-    }
-
-    -- Send config to alpha
-    alpha.setup(dashboard.opts)
-
-    -- Disable folding on alpha buffer
-    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
-  end,
+		require("alpha").setup(configure())
+	end,
 }
--- lua/drucial/plugins/colorscheme.lua
+++ lua/drucial/plugins/colorscheme.lua
@@ -1,3 +1,21 @@
+local colors = {
+	bg = "#030517", -- Updated to match the darker blue background
+	bgAlt = "#1e2124", -- Kept as it is
+	bgHighlight = "#3c4048", -- Kept as it is
+	cursor = "#ffffff", -- Kept as it is
+	fg = "#ffffff", -- Kept as it is
+	grey = "#7b8496", -- Kept as it is
+	blue = "#00FFFF", -- Kept as it is
+	green = "#ecff8d", -- Updated to lean towards yellow
+	cyan = "#5ef1ff", -- Kept as it is
+	red = "#ff8272", -- Kept as it is
+	yellow = "#ffbd5e", -- Updated to be more towards orange
+	magenta = "#ff8ffd", -- Kept as it is
+	pink = "#ff5ea0", -- Kept as it is
+	orange = "#ffbd5e", -- Kept as it is
+	purple = "#bd5eff", -- Kept as it is
+}
+
 return {
 	"scottmckendry/cyberdream.nvim",
 	lazy = false,
@@ -10,26 +28,9 @@ return {
 			hide_fillchars = true,
 			borderless_telescope = true,
 			terminal_colors = true,
-			theme = {
-				colors = {
-					bg = "#030517", -- Updated to match the darker blue background
-					bgAlt = "#1e2124", -- Kept as it is
-					bgHighlight = "#3c4048", -- Kept as it is
-					cursor = "#ffffff", -- Kept as it is
-					fg = "#ffffff", -- Kept as it is
-					grey = "#7b8496", -- Kept as it is
-					blue = "#00FFFF", -- Kept as it is
-					green = "#ecff8d", -- Updated to lean towards yellow
-					cyan = "#5ef1ff", -- Kept as it is
-					red = "#ff8272", -- Kept as it is
-					yellow = "#ffbd5e", -- Updated to be more towards orange
-					magenta = "#ff8ffd", -- Kept as it is
-					pink = "#ff5ea0", -- Kept as it is
-					orange = "#ffbd5e", -- Kept as it is
-					purple = "#bd5eff", -- Kept as it is
-				},
-			},
+			theme = { colors = colors },
 		})
 		vim.cmd("colorscheme cyberdream") -- set the colorscheme
 	end,
+	colors = colors,
 }
