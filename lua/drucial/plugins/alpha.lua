0de48ef91582d9da2d9ebf4833b48d83e3df7b8b
Drew White <32739358+Drucial@users.noreply.github.com>
2024-04-27 00:35:05 -0400

    splash screen update

--- lua/drucial/plugins/alpha.lua
+++ lua/drucial/plugins/alpha.lua
@@ -50,17 +50,12 @@ return {
 		}
 
 		dashboard.section.buttons.val = {
-			dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
-			dashboard.button("n", " " .. " New file", "<cmd> ene <BAR> startinsert <cr>"),
+			dashboard.button("e", " " .. " Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+			dashboard.button("s", " " .. " Restore Session", "<cmd>SessionRestore<cr>"),
 			dashboard.button("r", " " .. " Recent files", "<cmd> Telescope oldfiles <cr>"),
+			dashboard.button("n", " " .. " New file", "<cmd> ene <BAR> startinsert <cr>"),
+			dashboard.button("f", " " .. " Find file", "<cmd> Telescope find_files <cr>"),
 			dashboard.button("g", " " .. " Find text", "<cmd> Telescope live_grep <cr>"),
-			dashboard.button(
-				"c",
-				" " .. " Config",
-				"<cmd> lua require('lazyvim.util').telescope.config_files()() <cr>"
-			),
-			dashboard.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
-			dashboard.button("x", " " .. " Lazy Extras", "<cmd> LazyExtras <cr>"),
 			dashboard.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
 			dashboard.button("q", " " .. " Quit", "<cmd> qa <cr>"),
 		}
