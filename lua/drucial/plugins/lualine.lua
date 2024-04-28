94b1e16a1f1386d0c4894d8f06d5ddb82906fc90
Drew White <32739358+Drucial@users.noreply.github.com>
2024-04-28 09:27:50 -0400

    lualine theme updates

--- lua/drucial/plugins/lualine.lua
+++ lua/drucial/plugins/lualine.lua
@@ -10,28 +10,28 @@ return {
 		local my_lualine_theme = {
 			normal = {
 				a = { bg = colors.cyan, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bgAlt, fg = colors.fg },
-				c = { bg = colors.bgAlt, fg = colors.fg },
+				b = { bg = colors.bg, fg = colors.grey },
+				c = { bg = colors.bg, fg = colors.grey },
 			},
 			insert = {
 				a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bgAlt, fg = colors.fg },
-				c = { bg = colors.bgAlt, fg = colors.fg },
+				b = { bg = colors.bg, fg = colors.grey },
+				c = { bg = colors.bg, fg = colors.grey },
 			},
 			visual = {
 				a = { bg = colors.pink, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bgAlt, fg = colors.fg },
-				c = { bg = colors.bgAlt, fg = colors.fg },
+				b = { bg = colors.bg, fg = colors.grey },
+				c = { bg = colors.bg, fg = colors.grey },
 			},
 			command = {
 				a = { bg = colors.pink, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bgAlt, fg = colors.fg },
-				c = { bg = colors.bgAlt, fg = colors.fg },
+				b = { bg = colors.bg, fg = colors.grey },
+				c = { bg = colors.bg, fg = colors.grey },
 			},
 			replace = {
 				a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bgAlt, fg = colors.fg },
-				c = { bg = colors.bgAlt, fg = colors.fg },
+				b = { bg = colors.bg, fg = colors.grey },
+				c = { bg = colors.bg, fg = colors.grey },
 			},
 			inactive = {
 				a = { bg = colors.bg, fg = colors.grey, gui = "bold" },
