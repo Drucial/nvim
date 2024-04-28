0f4709b0df9ddc34f1038fe806d444745dc88ec3
Drew White <32739358+Drucial@users.noreply.github.com>
2024-04-27 20:22:52 -0400

    color theme finished

--- lua/drucial/plugins/alpha.lua
+++ lua/drucial/plugins/alpha.lua
@@ -8,37 +8,37 @@ return {
 		local colorscheme = require("drucial/plugins/colorscheme")
 		local colors = colorscheme.colors
 
-		vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = colors.blue })
-		vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = colors.green })
+		vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = colors.pink })
+		vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = colors.magenta })
 		vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = colors.cyan })
-		vim.api.nvim_set_hl(0, "NeovimDashboardLogo4", { fg = colors.green })
-		vim.api.nvim_set_hl(0, "NeovimDashboardLogo5", { fg = colors.blue })
+		vim.api.nvim_set_hl(0, "NeovimDashboardLogo4", { fg = colors.magenta })
+		vim.api.nvim_set_hl(0, "NeovimDashboardLogo5", { fg = colors.pink })
 
 		dashboard.section.header.type = "group"
 		dashboard.section.header.val = {
 			{
 				type = "text",
-				val = [[  Y88b Y88  888'Y88   e88 88e   Y88b Y88  ]],
+				val = [[  Y88b Y88  888'Y88   e88 88e   Y88b Y88    Y8b Y88888P  888      e   e     ]],
 				opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" },
 			},
 			{
 				type = "text",
-				val = [[   Y88b Y8  888 ,'Y  d888 888b   Y88b Y8  ]],
+				val = [[   Y88b Y8  888 ,'Y  d888 888b   Y88b Y8     Y8b Y888P   888     d8b d8b    ]],
 				opts = { hl = "NeovimDashboardLogo2", shrink_margin = false, position = "center" },
 			},
 			{
 				type = "text",
-				val = [[  b Y88b Y  888C8   C8888 8888D b Y88b Y  ]],
+				val = [[  b Y88b Y  888C8   C8888 8888D b Y88b Y      Y8b Y8P    888    e Y8b Y8b   ]],
 				opts = { hl = "NeovimDashboardLogo3", shrink_margin = false, position = "center" },
 			},
 			{
 				type = "text",
-				val = [[  8b Y88b   888 ",d  Y888 888P  8b Y88b   ]],
+				val = [[  8b Y88b   888 ",d  Y888 888P  8b Y88b        Y8b Y     888   d8b Y8b Y8b  ]],
 				opts = { hl = "NeovimDashboardLogo4", shrink_margin = false, position = "center" },
 			},
 			{
 				type = "text",
-				val = [[  88b Y88b  888,d88   "88 88"   88b Y88b  ]],
+				val = [[  88b Y88b  888,d88   "88 88"   88b Y88b        Y8P      888  d888b Y8b Y8b ]],
 				opts = { hl = "NeovimDashboardLogo5", shrink_margin = false, position = "center" },
 			},
 			-- [[-------------  *  --------------]],
--- lua/drucial/plugins/colorscheme.lua
+++ lua/drucial/plugins/colorscheme.lua
@@ -1,19 +1,19 @@
 local colors = {
-	bg = "#030517", -- Updated to match the darker blue background
-	bgAlt = "#1e2124", -- Kept as it is
-	bgHighlight = "#3c4048", -- Kept as it is
-	cursor = "#ffffff", -- Kept as it is
-	fg = "#ffffff", -- Kept as it is
-	grey = "#7b8496", -- Kept as it is
-	blue = "#00FFFF", -- Kept as it is
-	green = "#ecff8d", -- Updated to lean towards yellow
-	cyan = "#5ef1ff", -- Kept as it is
-	red = "#ff8272", -- Kept as it is
-	yellow = "#ffbd5e", -- Updated to be more towards orange
-	magenta = "#ff8ffd", -- Kept as it is
-	pink = "#ff5ea0", -- Kept as it is
-	orange = "#ffbd5e", -- Kept as it is
-	purple = "#bd5eff", -- Kept as it is
+	bg = "#030517",
+	bgAlt = "#1e2124",
+	bgHighlight = "#3c4048",
+	cursor = "#EDF2F6",
+	fg = "#FFFFFF",
+	grey = "#8894AD",
+	blue = "#00C2FF",
+	green = "#C0FE3D",
+	cyan = "#69FEE3",
+	red = "#FF4E4E",
+	yellow = "#FEEC4B",
+	magenta = "#FF3C77",
+	pink = "#FF3CA6",
+	orange = "#FFA620",
+	purple = "#A347FF",
 }
 
 return {
@@ -26,7 +26,7 @@ return {
 			transparent = true,
 			italic_comments = true,
 			hide_fillchars = true,
-			borderless_telescope = true,
+			borderless_telescope = false,
 			terminal_colors = true,
 			theme = { colors = colors },
 		})
