57583f9f5afddd1f64390947a5186b8c73597773
Drew White <32739358+Drucial@users.noreply.github.com>
2024-05-01 20:37:59 -0400

    remove lsp dir

--- lua/drucial/lazy.lua
+++ lua/drucial/lazy.lua
@@ -1,22 +1,24 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "drucial.plugins" }, { import = "drucial.plugins.lsp" } }, {
-  change_detection = {
-    checker = {
-      enabled = true,
-      notify = false,
-    },
-    notify = false,
-  },
+require("lazy").setup({
+	{ import = "drucial.plugins" },
+}, {
+	change_detection = {
+		checker = {
+			enabled = true,
+			notify = false,
+		},
+		notify = false,
+	},
 })
