5ffa7e454b12bc5977be478b39a0f612ff8992f9
Drew White <32739358+Drucial@users.noreply.github.com>
2024-05-03 08:33:55 -0400

    format on save with null-ls

--- lua/drucial/plugins/none-ls.lua
+++ lua/drucial/plugins/none-ls.lua
@@ -5,6 +5,7 @@ return {
 	},
 	config = function()
 		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 		null_ls.setup({
 			sources = {
@@ -15,9 +16,21 @@ return {
 				null_ls.builtins.formatting.rubocop,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.diagnostics.rubocop,
-				-- null_ls.builtins.formatting.erb_format,
-				-- null_ls.builtins.formatting.erb_lint,
+				null_ls.builtins.formatting.erb_format,
+				null_ls.builtins.formatting.erb_lint,
 			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = false, bufnr = bufnr })
+						end,
+					})
+				end
+			end,
 		})
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
