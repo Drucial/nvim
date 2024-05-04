74ecbbd52a56eeee50df1574b6416554603f2dc0
Drew White <32739358+Drucial@users.noreply.github.com>
2024-05-04 12:58:13 -0400

    Updated documentation for blankline and lazygit

--- lua/drucial/plugins/init.lua
+++ lua/drucial/plugins/init.lua
@@ -1,4 +1,7 @@
+-- List of Plugins
+-- This configuration file lists the plugins used in the Neovim setup.
+
 return {
-  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
+	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
 }
