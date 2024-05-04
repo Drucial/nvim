540f1513b6141b68b0e78a5b6f3cc665a54e068a
Drew White <32739358+Drucial@users.noreply.github.com>
2024-05-04 12:29:17 -0400

    all the git signs updates and some more documentation

--- lua/drucial/plugins/colorscheme.lua
+++ lua/drucial/plugins/colorscheme.lua
@@ -9,36 +9,48 @@
 --    You can adjust this example to set other colors or attributes as needed.
 
 local colors = {
-  bg = "#030517",
-  bgAlt = "#1e2124",
-  bgHighlight = "#3c4048",
-  cursor = "#EDF2F6",
-  fg = "#FFFFFF",
-  grey = "#8894AD",
-  blue = "#00C2FF",
-  green = "#C0FE3D",
-  cyan = "#69FEE3",
-  red = "#FF4E4E",
-  yellow = "#FEEC4B",
-  magenta = "#FF3C77",
-  pink = "#FF3CA6",
-  orange = "#FFA620",
-  purple = "#A347FF",
+	bg = "#030517",
+	bgAlt = "#1e2124",
+	bgHighlight = "#3c4048",
+	cursor = "#EDF2F6",
+	fg = "#FFFFFF",
+	grey = "#8894AD",
+	blue = "#00C2FF",
+	green = "#C0FE3D",
+	cyan = "#69FEE3",
+	red = "#FF4E4E",
+	yellow = "#FEEC4B",
+	magenta = "#FF3C77",
+	pink = "#FF3CA6",
+	orange = "#FFA620",
+	purple = "#A347FF",
+}
+
+local highlights = {
+	GitSignsCurrentLineBlame = { fg = colors.grey },
 }
 
 return {
-  "scottmckendry/cyberdream.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("cyberdream").setup({
-      transparent = true,
-      italic_comments = true,
-      borderless_telescope = false,
-      terminal_colors = true,
-      theme = { colors = colors },
-    })
-    vim.cmd("colorscheme cyberdream")
-  end,
-  colors = colors,
+	"scottmckendry/cyberdream.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("cyberdream").setup({
+			transparent = true,
+			italic_comments = true,
+			borderless_telescope = false,
+			terminal_colors = true,
+			theme = { colors = colors },
+		})
+
+		-- Apply highlight customizations
+		for group, attrs in pairs(highlights) do
+			local namespace = attrs.ns or 0
+			vim.api.nvim_set_hl(namespace, group, attrs)
+		end
+
+		-- Set the colorscheme
+		vim.cmd("colorscheme cyberdream")
+	end,
+	colors = colors,
 }
