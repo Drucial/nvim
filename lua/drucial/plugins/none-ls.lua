86b06545ea4bf190292e71bd7a694ba559d39ac3
Drew White <32739358+Drucial@users.noreply.github.com>
2024-05-09 08:44:30 -0400

    updated non-ls formatter for erb files

--- lua/drucial/plugins/none-ls.lua
+++ lua/drucial/plugins/none-ls.lua
@@ -7,39 +7,39 @@
 --   - <leader>gf: Trigger formatting for the current buffer.
 
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-    null_ls.setup({
-      sources = {
-        require("none-ls.diagnostics.eslint_d"),
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.rubocop,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.rubocop,
-        null_ls.builtins.formatting.erb_format,
-      },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ async = false, bufnr = bufnr })
-            end,
-          })
-        end
-      end,
-    })
+		null_ls.setup({
+			sources = {
+				require("none-ls.diagnostics.eslint_d"),
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.rubocop,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.rubocop,
+				-- null_ls.builtins.formatting.erb_lint,
+			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = false, bufnr = bufnr })
+						end,
+					})
+				end
+			end,
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
